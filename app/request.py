17453af6b5065627a3d2bbfd65abee8010bc9853
kilonzijnr <vkilonzi05@gmail.com>
2021-09-15 11:09:07 +0300

    imported casses from models

--- app/request.py
+++ app/request.py
@@ -1,5 +1,2 @@
-from app import app
-import urllib.request,json
-# from app.models.sources import Sources
-# from app.models.articles import Articles
- 
+import urllib.request, json
+from .models import Sources, Articles
