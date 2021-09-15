4d0ec8e27d2f04206858e7209b2598fbf25baeee
kilonzijnr <vkilonzi05@gmail.com>
2021-09-15 11:04:02 +0300

    defined source objects and created Article class

--- app/models.py
+++ app/models.py
@@ -2,4 +2,13 @@ class Sources:
     """
     A class to define sorces objects
     """
-    
+    def __init__(self, id, name, description, url):
+          self.id= id
+          self.name= name
+          self.description= description
+          self.url= url
+
+class Article:
+    """
+    A class to define article objects
+    """
