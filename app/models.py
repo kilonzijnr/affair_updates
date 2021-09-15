0df121748727bda996d766dd3eb9127fd8ac964a
kilonzijnr <vkilonzi05@gmail.com>
2021-09-15 11:07:13 +0300

    defined article objects

--- app/models.py
+++ app/models.py
@@ -12,3 +12,10 @@ class Article:
     """
     A class to define article objects
     """
+    def __init__(self, author, title, description, url, urlToImage, publishedAt):
+        self.author= author
+        self.title= title
+        self.description= description
+        self.url= url
+        self.urlToImage= urlToImage
+        self.publishedAt= publishedAt
