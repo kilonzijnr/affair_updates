8bfd7d560f4309e2009ac29b7739fd414b8591b2
kilonzijnr <vkilonzi05@gmail.com>
2021-09-20 16:09:17 +0300

    initialized app settings

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,30 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
+def create_app(config_name):
+   
+    #initializing application
+    app = Flask(__name__)
+
+    #setting up configuaration
+    app.config.from_object(config_options[config_name])
+
+    #initializing Flask Extensions
+    bootstrap.init_app(app)
+
+    #registering blueprints
+    from .main import main as main_blueprint
+
+    app.register_blueprint(main_blueprint)
+
+    #setting up config
+    from .request import config_request
+
+    config_request(app)
+
+    return app
+
+
+
 
 
